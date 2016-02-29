4cc68528c77f0db759a48d227d707ef3d8bb9b2e
asoccard <aurelien.soccard@epfl.ch>
2016-02-29 22:30:25 +0100

    Clear main stuff

--- mining/src/main/scala/bookshelf/mining/Main.scala
+++ mining/src/main/scala/bookshelf/mining/Main.scala
@@ -7,28 +7,24 @@ object Main extends App {
   def prettyPrint[T](data: List[T]): Unit =
     println(data.mkString("\n"))
 
-  val PATTERN = "(\\d*)\\+([A-z]*)".r
-
-  val abc = "1+a"
-
-  /*
   println("\n\n===== Authors =====")
   prettyPrint(Author.all.filter(_.isFailure))
+
   println("\n\n===== Publications =====")
-  val data = Publications.all
-  prettyPrint(data.filter(_.isFailure))
-    println("\n\n===== Publication authors =====")
-    prettyPrint(Publication_author.all.filter(_.isFailure))
+  prettyPrint(Publications.all.filter(_.isFailure))
 
-    println("\n\n===== Publication content =====")
-    prettyPrint(Publication_content.all.filter(_.isFailure))
+  println("\n\n===== Publication authors =====")
+  prettyPrint(Publication_author.all.filter(_.isFailure))
 
-    println("\n\n===== Publication series =====")
-    prettyPrint(Publication_serie.all.filter(_.isFailure))
+  println("\n\n===== Publication content =====")
+  prettyPrint(Publication_content.all.filter(_.isFailure))
 
-    println("\n\n===== Publishers =====")
-    prettyPrint(Publisher.all.filter(_.isFailure))
-    */
+  println("\n\n===== Publication series =====")
+  prettyPrint(Publication_serie.all.filter(_.isFailure))
+
+  println("\n\n===== Publishers =====")
+  prettyPrint(Publisher.all.filter(_.isFailure))
+
   println("\n\n===== Awards =====")
   prettyPrint(Award.all.filter(_.isFailure))
 
