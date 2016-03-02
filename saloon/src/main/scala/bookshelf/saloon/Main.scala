b440bc201b6912399ce7fca2ebdd12fd909ba27d
Teo Stocco <teo.stocco@epfl.ch>
2016-03-02 20:48:51 +0100

    create basic server

--- saloon/src/main/scala/bookshelf/saloon/Main.scala
+++ saloon/src/main/scala/bookshelf/saloon/Main.scala
@@ -1,20 +1,42 @@
 package bookshelf.saloon
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 
-object Main extends App {
+import scala.io.StdIn
+
+private[saloon] object Main extends App {
 
   val config = ConfigFactory.load()
 
-  implicit val system = ActorSystem()
-  implicit val executor = system.dispatcher
+  implicit val system = ActorSystem("Bookshelf-system")
   implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
 
   val routes =
+    path("q") {
+      get {
+        complete {
+          "hello"
+        }
+      }
+    } ~
+      pathSingleSlash {
+        getFromFile("static/index.html")
+      } ~
+      getFromDirectory("static")
 
-  Http().bindAndHandle(routes, config.getString("http.interface"), config.getInt("http.port"))
+
+  val bindingFuture = Http().bindAndHandle(logRequestResult("Bookshelf", Logging.InfoLevel)(routes), config.getString("http.interface"), config.getInt("http.port"))
+
+  println("Press ENTER to stop.")
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 
 }
