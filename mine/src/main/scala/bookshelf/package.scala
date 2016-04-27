6eb09cd5d4b41332864287f713bf83e87a8f2aca
Teo Stocco <teo.stocco@epfl.ch>
2016-04-27 14:29:57 +0200

    quickfix

--- mine/src/main/scala/bookshelf/package.scala
+++ mine/src/main/scala/bookshelf/package.scala
@@ -1,15 +1,9 @@
 import io.getquill._
-import io.getquill.sources.jdbc.JdbcSource
 import org.joda.time.DateTime
 
 package object bookshelf {
 
   implicit val encodeJoda = mappedEncoding[DateTime, java.util.Date](_.toDate)
   implicit val decodeJoda = mappedEncoding[java.util.Date, DateTime](new DateTime(_))
-  //implicit val encodeOptJoda = mappedEncoding[Option[DateTime], Option[java.util.Date]](_.map(_.toDate))
-  //implicit val decodeOptJoda = mappedEncoding[Option[java.util.Date], Option[DateTime]](_.map(new DateTime(_)))
-
-  //def truncate(table: String)(implicit db: JdbcSource[_, _]): Unit =
-  //  db.execute(s"TRUNCATE TABLE $table", None)
 
 }
