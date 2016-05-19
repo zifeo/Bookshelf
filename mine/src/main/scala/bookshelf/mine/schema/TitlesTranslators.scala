f7cb5f646893348e9007342b0029496d71fd9bdd
asoccard <aurelien.soccard@epfl.ch>
2016-05-19 18:49:56 +0200

    Fix parsing issues

--- mine/src/main/scala/bookshelf/mine/schema/TitlesTranslators.scala
+++ mine/src/main/scala/bookshelf/mine/schema/TitlesTranslators.scala
@@ -5,10 +5,10 @@ import bookshelf.mine._
 import scala.util.Try
 
 case class TitlesTranslators(
-                             titleId: Int,
-                             translatorId: Int,
-                             year: Int,
-                             language: String
+                              titleId: Int,
+                              translatorId: Int,
+                              year: Int,
+                              languageId: Int
                            )
 
 object TitlesTranslators {
@@ -22,15 +22,23 @@ object TitlesTranslators {
     visited.indexOf(name) + 1
   }
 
+  def getLanguageId(raw:String) : Int = raw.toLowerCase match {
+    case "english" => 17
+    case "italian" => 36
+    case "romanian" => 54
+    case "russian" => 55
+    case _ => 0
+  }
+
   def parseCols(raw: List[String]): Try[List[TitlesTranslators]] = Try {
     raw match {
       case List(id, _, translator, _, _, _, _, _, _, _, _, _) =>
         translator.split(';').map { t => t.split(',').toList match {
-          case List(language, year, name) => TitlesTranslators(
+          case List(languageId, year, name) => TitlesTranslators(
             id.toInt,
             inc(name),
             year.toInt,
-            language
+            getLanguageId(languageId)
           )
         }
         }.toList
